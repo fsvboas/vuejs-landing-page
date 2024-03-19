7f278c2330dad2709532a22eb5dc69e0f6923520
fsvboas <fsvboas.dev@gmail.com>
2024-03-19 18:45:41 -0300

    feat: create marquee component using vue3-marquee library

---
+++ src/components/Marquee.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+  props: {
+    images: {
+      type: Array<{ name: string; imageUrl: string }>,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <Vue3Marquee>
+    <img
+      class="select-none"
+      v-for="(image, index) in images"
+      :src="image?.imageUrl"
+      :alt="image?.name"
+      :key="index"
+    />
+  </Vue3Marquee>
+</template>
