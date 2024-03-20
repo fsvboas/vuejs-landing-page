28d589ee7ba97ecb892de4e67854f97c057574e1
fsvboas <fsvboas.dev@gmail.com>
2024-03-20 15:26:58 -0300

    feat: create app features section component

---
+++ src/components/FeatureCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Images } from "lucide-vue-next";
+
+const props = defineProps<{
+  title: string;
+  resume: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center h-fit w-fit p-4 space-y-4"
+  >
+    <Images color="white" :size="36" />
+    <h3 class="text-white text-2xl">{{ props?.title }}</h3>
+    <p class="text-white text-lg text-center max-w-[400px]">
+      {{ props?.resume }}
+    </p>
+  </div>
+</template>
---
+++ src/components/Features.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import FeatureCard from "./FeatureCard.vue";
+
+const props = defineProps<{
+  features: Array<{ title: string; resume: string }>;
+}>();
+</script>
+
+<template>
+  <div
+    ref="featuresRef"
+    class="w-full h-full p-20 bg-green-600 grid grid-cols-3 grid-rows-2 gap-10 place-items-center"
+  >
+    <FeatureCard
+      v-for="(feature, index) in props?.features"
+      :title="feature.title"
+      :resume="feature?.resume"
+      :key="index"
+    />
+  </div>
+</template>
