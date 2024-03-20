cd9ca03427459195a559170dd35fa373b0449003
fsvboas <fsvboas.dev@gmail.com>
2024-03-20 15:28:07 -0300

    refactor: update HomeView with the new components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import About from "@/components/About.vue";
 import Billboard from "@/components/Billboard.vue";
+import Features from "@/components/Features.vue";
+import Footer from "@/components/Footer.vue";
 import Marquee from "@/components/Marquee.vue";
 import Navbar from "@/components/Navbar.vue";
-
 const sponsors = [
   {
     name: "Google",
@@ -22,6 +23,39 @@ const sponsors = [
     imageUrl: "/src/assets/sponsors/paypal.png",
   },
 ];
+
+const features = [
+  {
+    title: "MALESUADA FAMES TURPIS.",
+    resume:
+      "vel ultrices mauris libero id diam. Vivamus tellus sagittis facilisis nisi quis mollis risus quisque ultrices elit.",
+  },
+  {
+    title: "MALESUADA FAMES TURPIS.",
+    resume:
+      "vel ultrices mauris libero id diam. Vivamus tellus sagittis facilisis nisi quis mollis risus quisque ultrices elit.",
+  },
+  {
+    title: "MALESUADA FAMES TURPIS.",
+    resume:
+      "vel ultrices mauris libero id diam. Vivamus tellus sagittis facilisis nisi quis mollis risus quisque ultrices elit.",
+  },
+  {
+    title: "MALESUADA FAMES TURPIS.",
+    resume:
+      "vel ultrices mauris libero id diam. Vivamus tellus sagittis facilisis nisi quis mollis risus quisque ultrices elit.",
+  },
+  {
+    title: "MALESUADA FAMES TURPIS.",
+    resume:
+      "vel ultrices mauris libero id diam. Vivamus tellus sagittis facilisis nisi quis mollis risus quisque ultrices elit.",
+  },
+  {
+    title: "MALESUADA FAMES TURPIS.",
+    resume:
+      "vel ultrices mauris libero id diam. Vivamus tellus sagittis facilisis nisi quis mollis risus quisque ultrices elit.",
+  },
+];
 </script>
 
 <template>
@@ -29,4 +63,6 @@ const sponsors = [
   <Billboard />
   <Marquee :images="sponsors" />
   <About />
+  <Features :features="features" />
+  <Footer />
 </template>
